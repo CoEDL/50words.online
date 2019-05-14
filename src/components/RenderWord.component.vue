1faafbdd8f3acac864a0e00ecbc498dacb1a41e0
Marco La Rosa <m@lr.id.au>
2019-05-14 16:45:35 +1000

    DRY up and style based on usage

--- src/components/RenderWord.component.vue
+++ src/components/RenderWord.component.vue
@@ -1,22 +1,49 @@
 <template>
     <div>
-        <div class="row style-word-row">
-            <div class="col-1">
-                <el-button
-                    type="text"
-                    class="style-button"
-                    @click="playWord"
-                    v-if="word.audio_file"
-                >
-                    <i class="fas fa-volume-up"></i>
-                </el-button>
-                <audio ref="audioElement" v-if="word.audio_file">
-                    <source :src="word.audio_file">Your browser does not support the
-                    <code>audio</code> element.
-                </audio>
-            </div>
-            <div class="col-5">{{word.english}}</div>
-            <div class="col-6">{{word.indigenous}}</div>
+        <div class="style-word-row">
+            <span v-if="layout === 'popup'">
+                <div class="row">
+                    <div class="col-4">
+                        <el-button
+                            type="text"
+                            class="style-button px-3"
+                            @click="playWord"
+                            v-if="word.audio_file"
+                        >
+                            <i class="fas fa-volume-up fa-2x"></i>
+                        </el-button>
+                        <audio ref="audioElement" v-if="word.audio_file">
+                            <source :src="word.audio_file">Your browser does not support the
+                            <code>audio</code> element.
+                        </audio>
+                    </div>
+                    <div class="col-8">{{word.english}}</div>
+                </div>
+                <div class="row">
+                    <div class="col-4"></div>
+                    <div class="col-8">{{word.indigenous}}</div>
+                </div>
+            </span>
+            <span v-if="layout !=='popup'">
+                <div class="row">
+                    <div class="col-2">
+                        <el-button
+                            type="text"
+                            class="style-button px-3"
+                            @click="playWord"
+                            v-if="word.audio_file"
+                        >
+                            <i class="fas fa-volume-up"></i>
+                        </el-button>
+                        <audio ref="audioElement" v-if="word.audio_file">
+                            <source :src="word.audio_file">Your browser does not support the
+                            <code>audio</code> element.
+                        </audio>
+                    </div>
+                    <div class="col-5">{{word.english}}</div>
+                    <div class="col-5">{{word.indigenous}}</div>
+                </div>
+            </span>
         </div>
     </div>
 </template>
@@ -24,15 +51,27 @@
 <script>
 export default {
     props: {
+        layout: String,
         word: Object
     },
     data() {
-        return {};
+        return {
+            watchers: {}
+        };
+    },
+    mounted() {
+        this.watchers.audio = this.$watch("word.audio_file", () => {
+            console.log("changed");
+            if (this.$refs.audioElement) this.$refs.audioElement.load();
+        });
+    },
+    beforeDestroy() {
+        if (this.watchers.audio) this.watchers.audio();
     },
     methods: {
         playWord() {
             setTimeout(() => {
-                this.$refs["audioElement"].play();
+                this.$refs.audioElement.play();
             }, 500);
         }
     }
@@ -42,7 +81,6 @@ export default {
 <style lang="scss" scoped>
 .style-word-row {
     line-height: 40px;
-    border-bottom: 1px solid #000;
 }
 </style>
 
