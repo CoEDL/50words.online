b5e31195ac75cb3b0a68607d9219189ef8441250
Marco La Rosa <m@lr.id.au>
2019-05-14 16:27:47 +1000

    add words layer and controls to swap between words and languages

--- src/components/Map.component.vue
+++ src/components/Map.component.vue
@@ -2,22 +2,51 @@
     <div>
         <div id="map" class="style-map"></div>
         <div class="style-map-reset">
-            <el-button type="primary" circle @click="centerMap">
-                <i class="fas fa-crosshairs"></i>
-            </el-button>
+            <div class="row">
+                <div class="col">
+                    <el-button type="primary" circle @click="centerMap">
+                        <i class="fas fa-crosshairs"></i>
+                    </el-button>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col">
+                    <el-button type="primary" circle @click="addLanguageLayer">
+                        <i class="fas fa-undo"></i>
+                    </el-button>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import Vue from "vue";
+import ElementUI from "element-ui";
+import locale from "element-ui/lib/locale/lang/en";
+Vue.use(ElementUI, { locale });
+import RenderWordComponent from "components/RenderWord.component.vue";
 import mapboxgl from "mapbox-gl";
 mapboxgl.accessToken =
-    "pk.eyJ1IjoibWFyY29sYXJvc2EiLCJhIjoiY2pvM2pjMW9kMHhmODNxcmxsMTd2cWkzcCJ9.jpWvN4mzM5M6ijwkSI2CfA";
+    "pk.eyJ1IjoibWFyY29sYXJvc2EiLCJhIjoiY2p2NGhzMzFkMnFsbzQwandhNmJ2MWI2eCJ9.qxu6U_HfPFd-4BZ85eNCvw";
 import { throttle, map } from "lodash";
 
 export default {
+    components: {
+        RenderWordComponent
+    },
     data() {
         return {};
+    },
+    computed: {
+        words: function() {
+            return this.$store.state.selectedWord;
+        }
+    },
+    watch: {
+        words: function() {
+            this.renderWordLayer();
+        }
     },
     mounted() {
         this.map = new mapboxgl.Map({
@@ -35,6 +64,26 @@ export default {
             this.$store.commit("setSelectedLanguage", {
                 ...e.features[0].properties
             });
+        });
+        this.map.on("click", "words", e => {
+            const RenderWordClass = Vue.extend(RenderWordComponent);
+            const popup = new mapboxgl.Popup()
+                .setLngLat(e.lngLat)
+                .setHTML('<div id="vue-popup-content"></div>')
+                .addTo(this.map);
+            const popupInstance = new RenderWordClass({
+                propsData: {
+                    word: {
+                        english: "goodbye/I'll see you later",
+                        indigenous: "marndaj",
+                        audio_file: "/repository/N36/N36_goodbye.wav",
+                        language: "Bilinarra",
+                        code: "N36"
+                    }
+                }
+            });
+            popupInstance.$mount("#vue-popup-content");
+            popup._update();
         });
         // Change the cursor to a pointer when the mouse is over the places layer.
         this.map.on("mouseenter", "languages", () => {
@@ -57,6 +106,9 @@ export default {
             this.map.fitBounds([[96, -45], [168, -8]]);
         },
         addLanguageLayer() {
+            this.$store.commit("unsetSelectedWord");
+            if (this.map.getLayer("words"))
+                this.map.setLayoutProperty("words", "visibility", "none");
             const features = this.$store.state.languages.map(language => {
                 return {
                     type: "Feature",
@@ -69,23 +121,71 @@ export default {
                     }
                 };
             });
-            this.map.addLayer({
-                id: "languages",
-                type: "symbol",
-                source: {
-                    type: "geojson",
-                    data: {
-                        type: "FeatureCollection",
-                        features
+            // console.log(JSON.stringify(features, null, 2));
+            if (!this.map.getLayer("languages")) {
+                this.map.addLayer({
+                    id: "languages",
+                    type: "symbol",
+                    source: {
+                        type: "geojson",
+                        data: {
+                            type: "FeatureCollection",
+                            features
+                        }
+                    },
+                    layout: {
+                        visibility: "visible"
+                    },
+                    paint: {
+                        "text-color": "#f15a22"
+                    },
+                    layout: {
+                        "text-field": "{name}"
                     }
-                },
-                paint: {
-                    "text-color": "#f15a22"
-                },
-                layout: {
-                    "text-field": "{name}"
-                }
+                });
+            }
+            this.map.setLayoutProperty("languages", "visibility", "visible");
+        },
+        renderWordLayer() {
+            if (!this.words) return;
+            if (this.map.getLayer("languages"))
+                this.map.setLayoutProperty("languages", "visibility", "none");
+            const features = this.words.map(word => {
+                return {
+                    type: "Feature",
+                    properties: {
+                        ...word
+                    },
+                    geometry: {
+                        type: "Point",
+                        coordinates: [word.lng, word.lat]
+                    }
+                };
             });
+            // console.log(JSON.stringify(features, null, 2));
+
+            if (!this.map.getLayer("words")) {
+                this.map.addSource("words", {
+                    type: "geojson",
+                    data: { type: "FeatureCollection", features }
+                });
+                this.map.addLayer({
+                    id: "words",
+                    type: "symbol",
+                    source: "words",
+                    paint: {
+                        "text-color": "#f15a22"
+                    },
+                    layout: {
+                        "text-field": "{indigenous}"
+                    }
+                });
+            } else {
+                this.map
+                    .getSource("words")
+                    .setData({ type: "FeatureCollection", features });
+            }
+            this.map.setLayoutProperty("words", "visibility", "visible");
         }
     }
 };
@@ -107,5 +207,3 @@ export default {
     left: calc(100vw - 50px);
 }
 </style>
-
-
