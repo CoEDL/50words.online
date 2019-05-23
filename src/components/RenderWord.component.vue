0a53df100ac517af15ddf3683430849565f71307
Marco La Rosa <m@lr.id.au>
2019-05-23 10:38:16 +1000

    restyle word row in information panel

--- src/components/RenderWord.component.vue
+++ src/components/RenderWord.component.vue
@@ -28,23 +28,29 @@
                 </div>-->
             </span>
             <span v-if="layout !=='popup'">
-                <div class="row">
-                    <div class="col-2">
-                        <el-button
-                            type="text"
-                            class="style-button px-3"
-                            @click="playWord"
-                            v-if="word.audio_file"
-                        >
-                            <i class="fas fa-volume-up"></i>
-                        </el-button>
-                        <audio ref="audioElement" v-if="word.audio_file">
-                            <source :src="word.audio_file">Your browser does not support the
-                            <code>audio</code> element.
-                        </audio>
+                <div class="my-4">
+                    <div class="row">
+                        <div class="col-2">
+                            <el-button
+                                type="text"
+                                class="style-button px-3"
+                                @click="playWord"
+                                v-if="word.audio_file"
+                            >
+                                <i class="fas fa-volume-up fa-2x"></i>
+                            </el-button>
+                            <audio ref="audioElement" v-if="word.audio_file">
+                                <source :src="word.audio_file">Your browser does not support the
+                                <code>audio</code> element.
+                            </audio>
+                        </div>
+                        <div class="col-10">
+                            <div class="row">
+                                <div class="col-12 style-english">{{word.english}}</div>
+                                <div class="col-12 style-indigenous">{{word.indigenous}}</div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-5">{{word.english}}</div>
-                    <div class="col-5">{{word.indigenous}}</div>
                 </div>
             </span>
         </div>
@@ -84,6 +90,16 @@ export default {
 <style lang="scss" scoped>
 .style-word-row {
     line-height: 40px;
+}
+
+.style-english {
+    opacity: 0.8;
+    font-size: 1.5em;
+}
+
+.style-indigenous {
+    border-bottom: 1px solid #000;
+    font-size: 2em;
 }
 </style>
 
