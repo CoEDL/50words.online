da860a3d607b8c3ba8f041a385ff6bf7861cfd73
Marco La Rosa <m@lr.id.au>
2019-06-23 09:48:49 +1000

    implement play / stop control

--- src/components/RenderSelectedWord.component.vue
+++ src/components/RenderSelectedWord.component.vue
@@ -1,17 +1,42 @@
 <template>
-    <div class="style-selected-word text-right">{{word}}</div>
+    <div v-if="word">
+        <div class="style-selected-word text-right">
+            <el-button type="text" @click="play" class="style-audio-control">
+                <span v-show="!playAll">
+                    <i class="fas fa-play fa-2x fa-fw"></i>
+                    &nbsp;
+                </span>
+                <span v-show="playAll">
+                    <i class="fas fa-stop fa-2x fa-fw"></i>
+                    &nbsp;
+                </span>
+            </el-button>
+            {{word}}
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
     data() {
-        return {};
+        return {
+            isPlaying: false
+        };
     },
     computed: {
+        playAll: function() {
+            return this.$store.state.playAll.play;
+        },
         word: function() {
             return this.$store.state.selectedWord
                 ? this.$store.state.selectedWord[0].english
                 : this.$store.state.selectedWord;
+        }
+    },
+    methods: {
+        play() {
+            this.isPlaying = !this.isPlaying;
+            this.$store.commit("setPlayAll", this.isPlaying);
         }
     }
 };
@@ -26,7 +51,6 @@ export default {
 }
 @media (min-width: 1024px) {
     .style-selected-word {
-        font-size: 2.5em;
         color: $text-color;
     }
 }
