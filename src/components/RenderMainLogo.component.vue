042eaec09129df4c5ce0525f16f6a6ec84a378f7
Marco La Rosa <m@lr.id.au>
2019-11-19 16:33:00 +1100

    adjust / hide logos on small devices

--- src/components/RenderMainLogo.component.vue
+++ src/components/RenderMainLogo.component.vue
@@ -12,12 +12,12 @@
                 <br />INDIGENOUS LANGUAGE
             </div>
         </a>
-        <div class="style-firstlang-logo-container">
+        <div class="style-firstlang-logo-container" v-if="!smallDevice">
             <a href="https://www.firstlanguages.org.au" target="_blank">
                 <img :src="firstLangLogo" class="style-logo" />
             </a>
         </div>
-        <div class="style-uom-logo-container">
+        <div class="style-uom-logo-container" v-if="!smallDevice">
             <a href="https://www.unimelb.edu.au" target="_blank">
                 <img :src="uomLogo" class="style-logo" />
             </a>
@@ -38,6 +38,11 @@ export default {
             uomLogo: require("src/assets/UOMLogo.jpg"),
             firstLangLogo: require("src/assets/firstlanguages.png")
         };
+    },
+    computed: {
+        smallDevice: function() {
+            return window.innerWidth < 700 ? true : false;
+        }
     }
 };
 </script>
@@ -45,7 +50,7 @@ export default {
 <style lang="scss" scoped>
 .style-logo-container {
     position: relative;
-    width: 70px;
+    width: 50px;
 }
 .style-uom-logo-container {
     position: relative;
@@ -79,9 +84,9 @@ export default {
     position: absolute;
     font-family: times, serif;
     top: 0px;
-    width: 210px;
-    left: 80px;
-    font-size: 1.2em;
+    width: 150px;
+    left: 60px;
+    font-size: 0.8em;
     color: white;
 }
 @media (min-width: 768px) {
@@ -142,5 +147,3 @@ export default {
     }
 }
 </style>
-
-
