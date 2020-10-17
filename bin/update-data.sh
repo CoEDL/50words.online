cc0229b9b30b37d01103523846d4d0ef507fb481
Marco La Rosa <m@lr.id.au>
2020-10-17 14:19:02 +1100

    revise repo paths; add option to create staging repo

--- bin/update-data.sh
+++ bin/update-data.sh
@@ -3,16 +3,20 @@
 [[ "$2" == "--update-all" ]] && export UPDATE_ALL=true
 if [ "$1" == "--prod" ] ; then
     export DATA_50WORDS="/srv/data"
-    export REPOSITORY_50WORDS="/srv/50words.online"
+    export REPOSITORY_50WORDS="/srv/production"
     export LOG="INFO"
     docker-compose up
+elif [ "$1" == "--staging" ] ; then
+    export DATA_50WORDS="/srv/data"
+    export REPOSITORY_50WORDS="/srv/staging"
+    export LOG="INFO"
 elif [ "$1" == "--dev" ] ; then
     export DATA_50WORDS="./data"
     export REPOSITORY_50WORDS="./dist"
     export LOG="DEBUG"
     docker-compose up
 else
-    echo "Usage: $0 [ --prod | --dev ] [ --update-all ]"
+    echo "Usage: $0 [ --prod | --staging | --dev ] [ --update-all ]"
 fi
 
 
