1063afc4c3dd005b45bd8ab68f31ee78438ff60d
Marco La Rosa <m@lr.id.au>
2023-05-16 17:56:41 +1000

    closes #13

--- src/components/Help.component.vue
+++ src/components/Help.component.vue
@@ -1,5 +1,17 @@
 <template>
-    <div class="flex flex-col">
+    <div class="flex flex-col space-y-2">
+        <div class="text-2xl">Learning Resources</div>
+        <div class="break-normal">
+            Download a set of learning activities to accompany the 50 Words map. These are designed
+            for students of all ages and can be used at home or in the classroom.
+        </div>
+        <a
+            class="text-blue-700"
+            href="https://arts.unimelb.edu.au/__data/assets/pdf_file/0005/3352838/50_Words-Lockdown-activities.pdf"
+            target="_blank"
+            >Find out more.</a
+        >
+        <div class="text-2xl">How to use the site</div>
         <video controls class="hidden md:block">
             <source :src="helpVideoDesktop" type="video/mp4" />
         </video>
