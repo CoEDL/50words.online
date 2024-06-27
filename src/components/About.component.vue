9eb0b8ab3ee1540b86d2dc66ffd6589d7584f343
nthieberger <thien@unimelb.edu.au>
2024-06-27 16:03:05 +1000

    Update About.component.vue
    small corrections

--- src/components/About.component.vue
+++ src/components/About.component.vue
@@ -45,7 +45,7 @@
             Wik-Mungkan, older brother and younger brother, so we have included just one: “wuny”
             older brother.
         </p>
-        <p>Note that there can be variation in the way that words are pronounced, so the version on this site may not be exactly the same as that used by other speakers.
+        <p>Note that there can be variation in the way that words are pronounced, so the version on this site may not be exactly the same as that used by other speakers.</p>
         <p>
             All words, audio, and video recordings are provided by language speakers and are
             included here with permission. Australian Indigenous languages have many thousands of
@@ -65,8 +65,8 @@
                 class="style-link text-blue-600"
                 href="https://arts.unimelb.edu.au/research-unit-for-indigenous-language/research/current-research-projects/50-words-project"
                 target="_blank"
-                >For more information and resources see our project website.</a
-            > <a href="https://arts.unimelb.edu.au/__data/assets/pdf_file/0005/3352838/50Words_A5ActivityBooklet-V3-.pdf">A teaching guide can be downloaded here. </a>
+                >For more information and resources see our project website. </a
+            > <a href="https://arts.unimelb.edu.au/__data/assets/pdf_file/0005/3352838/50Words_A5ActivityBooklet-V3-.pdf"> A teaching guide can be downloaded here. </a>
         </p>
         <render-about-logo-component />
     </div>
