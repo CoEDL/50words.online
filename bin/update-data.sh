8c81a2c00b4bbb5e7a00bf8b3e31cda685cd9729
John Ferlito <johnf@inodes.org>
2026-06-17 13:45:14 +1000

    feat: update for new server paths

--- bin/update-data.sh
+++ bin/update-data.sh
@@ -2,20 +2,20 @@
 
 [[ "$2" == "--update-all" ]] && export UPDATE_ALL=true
 if [ "$1" == "--prod" ] ; then
-    export DATA_50WORDS="/srv/data"
-    export REPOSITORY_50WORDS="/srv/production"
+    export DATA_50WORDS="/srv/docker/50words/data"
+    export REPOSITORY_50WORDS="/srv/docker/50words/production"
     export LOG="INFO"
-    docker-compose up
+    docker compose up
 elif [ "$1" == "--staging" ] ; then
     export DATA_50WORDS="/srv/data"
     export REPOSITORY_50WORDS="/srv/staging"
     export LOG="INFO"
-    docker-compose up
+    docker compose up
 elif [ "$1" == "--dev" ] ; then
     export DATA_50WORDS="./data"
     export REPOSITORY_50WORDS="./dist"
     export LOG="DEBUG"
-    docker-compose up
+    docker compose up
 else
     echo "Usage: $0 [ --prod | --staging | --dev ] [ --update-all ]"
 fi
